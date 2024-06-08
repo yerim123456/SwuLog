f272c8f90ba5dc708ad1056ce4218507340bd3cc
Seoheeuhm <seohee030128@naver.com>
2024-06-08 14:59:39 +0900

    signup and login/logout commit.

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,10 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 
-from accountapp.views import hello_world
+from accountapp.views import hello_world, SignUpView
 
 app_name = "accountapp"
 
 urlpatterns = [
     path('hello_world/', hello_world, name='hello_world'),
+    path('signup/', SignUpView.as_view(), name='signup'),
+    path('login/', LoginView.as_view(template_name='accountapp/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
